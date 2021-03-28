69e7131cee58d134e8e2c9418e847e124813a524
Juan Carlos Ballesteros <jcballesteroshermida@gmail.com>
2021-03-28 16:36:52 +0200

    Usuario Tecnificado

--- src/main/java/org/tecnificados/boot/WebSecurityConfig.java
+++ src/main/java/org/tecnificados/boot/WebSecurityConfig.java
@@ -8,10 +8,15 @@ package org.tecnificados.boot;
 
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.tecnificados.boot.controller.StartController;
 
 
@@ -41,6 +46,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 			.logout()
 				.permitAll();
+		
+	}
+	
+
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("Tecnificado")
+				.password("ironman")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
 	}
 
 	
